c11cb295eeed2dc6f373de615a915db885017864
Júnior <anibalsidneyjr@hotmail.com>
2019-09-04 19:51:20 -0300

    Projeto meu primeiro app

---
+++ num1/app/src/main/java/br/senac/go/num1/MainActivity.java
@@ -0,0 +1,28 @@
+package br.senac.go.num1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+//Button.setOnClick(new View.OnClickListener() {
+//        public void onClick(View v) {
+//            Intent it = new Intent(MainActivity.this, Main2Activity.class);
+//            startActivity(it);
+//        }
+//    });
+
+}
